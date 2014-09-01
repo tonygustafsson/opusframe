0ade0957db2219cc5cd5f43ac17b888841800a55
Tony Gustafsson <mail@tonyg.se>
2014-09-01 16:13:47 +0200

    Getting the images is much easier with client_id instead of getting a token. The module can now save the images and thumbnails to disk.

--- modules/instagram/instagram.module.php
+++ modules/instagram/instagram.module.php
@@ -7,97 +7,17 @@
 			$this->opus =& opus::$instance;
 			$this->url_accessible = array('authorize', 'save_token', 'save_images');
 
-			$this->token_path = $this->opus->config->base_path_absolute . "/cache/instagram/access_token";
 			$this->image_path = $this->opus->config->base_path_absolute . "/assets/images/instagram/";
-
 			$this->user_id = '1336819';
 			$this->client_id = '3df4102f06814637b7f660639b409fa0';
-			$this->client_secret = '0a6febb1ba7d4d18a34719210971184a';
-			$this->redirect_uri = 'http://www.tonyg.se/projects/opusframe/instagram/save_token/';
-			$this->response_type = 'code';
-
-			$this->auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' . $this->client_id . '&redirect_uri=' . urlencode($this->redirect_uri) . '&response_type=' . $this->response_type;
-			$this->token_url = 'https://api.instagram.com/oauth/access_token/';
-			$this->media_url = 'https://api.instagram.com/v1/users/' . $this->user_id . '/media/recent';
-		}
-
-		public function authorize()
-		{
-			//Use credentials to auth and redirect to save_token()
-			$ch = curl_init($this->auth_url);
-
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-			$data = curl_exec($ch);
-
-			echo '<pre>'; print_r( curl_getinfo($ch) ); 
-			echo htmlspecialchars($data);
-			exit;
-
-			curl_close($ch);
-
-			echo $data;
-		}
-
-		public function save_token()
-		{
-			//Get the token and save to disk
-			$code = $_GET['code'];
-
-			$query = array( 'client_id' => $this->client_id,
-							'client_secret' => $this->client_secret,
-							'grant_type' => 'authorization_code',
-							'redirect_uri' => urlencode($this->redirect_uri),
-							'code' => $code
-						);
-
-			$ch = curl_init($this->token_url);
-
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-			$data = curl_exec($ch);
-
-			curl_close($ch);
-
-			$json = json_decode($data);
-
-			$access_token = $json->access_token;
-
-			if (! empty($access_token))
-			{
-				$file = fopen($this->token_path, "w");
-				fwrite($file, $access_token);
-				fclose($file);
-			}
-		}
-
-		public function get_token()
-		{
-			//Get token from disk
-			$file = fopen($this->token_path, "r");
-			$token = fread($file, filesize($file));
-			fclose($file);
-
-			return trim($token);
+			$this->media_url = 'https://api.instagram.com/v1/users/' . $this->user_id . '/media/recent?client_id=' . $this->client_id;
 		}
 
 		public function save_images()
 		{
 			//Get the JSON from the API and save images that is not already saved
-			$query = array( 'access_token' => get_token());
-
 			$ch = curl_init($this->media_url);
 
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -109,7 +29,38 @@
 
 			$json = json_decode($data);
 
-			echo $json;
+			foreach ($json->data as $image)
+			{
+				$thumbnail_url = $image->images->thumbnail->url;
+				$large_image_url = $image->images->standard_resolution->url;
+
+				$thumbnail_path = $this->image_path . basename($thumbnail_url);
+				$large_image_path = $this->image_path . basename($large_image_url);
+				
+				$this->save_image($thumbnail_url, $thumbnail_path);
+				$this->save_image($large_image_url, $large_image_path);
+			}
+		}
+
+		public function save_image($url, $path)
+		{
+			if (! file_exists($path))
+			{
+				//Image is not saved yet
+				$ch = curl_init($url);
+
+				curl_setopt($ch, CURLOPT_HEADER, false);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+
+				$raw = curl_exec($ch);
+
+				curl_close($ch);
+
+				$fp = fopen($path, 'x');
+				fwrite($fp, $raw);
+				fclose($fp);
+			}
 		}
 
 		public function get_images()
