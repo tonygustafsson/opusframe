6d80f0d3c497176f2d40c776b5cafb38b9b163f5
Tony Gustafsson <mail@tonyg.se>
2014-09-04 16:12:24 +0200

    Possibility to get images with min_id and max_id. Ignoring videos. Saving created_time and caption in metadata files that can be read by get_images, that deliveres a nice array with all images.

--- modules/instagram/instagram.module.php
+++ modules/instagram/instagram.module.php
@@ -5,18 +5,29 @@
 		public function __construct()
 		{	
 			$this->opus =& opus::$instance;
-			$this->url_accessible = array('authorize', 'save_token', 'save_images');
+			$this->url_accessible = array('authorize', 'save_token', 'save_images', 'get_images');
 
 			$this->image_path = $this->opus->config->base_path_absolute . "/assets/images/instagram/";
 			$this->user_id = '1336819';
+			$this->number_of_images = 20; //Max 33
 			$this->client_id = '3df4102f06814637b7f660639b409fa0';
-			$this->media_url = 'https://api.instagram.com/v1/users/' . $this->user_id . '/media/recent?client_id=' . $this->client_id;
+			$this->media_url = 'https://api.instagram.com/v1/users/' . $this->user_id . '/media/recent?client_id=' . $this->client_id . '&count=' . $this->number_of_images;
 		}
 
 		public function save_images()
 		{
+			$media_url = $this->media_url;
+
+			if (isset($_GET['min_id']))
+				$media_url .= '&min_id=' . $_GET['min_id'];
+
+			if (isset($_GET['max_id']))
+				$media_url .= '&max_id=' . $_GET['max_id'];
+
+			echo $media_url;
+
 			//Get the JSON from the API and save images that is not already saved
-			$ch = curl_init($this->media_url);
+			$ch = curl_init($media_url);
 
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -31,14 +42,23 @@
 
 			foreach ($json->data as $image)
 			{
+				if ($image->type != "image")
+					continue;
+
+				$image_id = $image->id;
 				$thumbnail_url = $image->images->thumbnail->url;
 				$large_image_url = $image->images->standard_resolution->url;
+				$created_time = $image->created_time;
+				$caption = $image->caption->text;
+				$metadata = $created_time . '###' . $caption;
 
-				$thumbnail_path = $this->image_path . basename($thumbnail_url);
-				$large_image_path = $this->image_path . basename($large_image_url);
+				$metadata_path = $this->image_path . $image_id . '.txt';
+				$thumbnail_path = $this->image_path . $image_id . '-thumb.jpg';
+				$large_image_path = $this->image_path . $image_id . '.jpg';
 				
 				$this->save_image($thumbnail_url, $thumbnail_path);
 				$this->save_image($large_image_url, $large_image_path);
+				$this->save_metadata($metadata_path, $metadata);
 			}
 		}
 
@@ -63,12 +83,61 @@
 			}
 		}
 
+		public function save_metadata($path, $metadata)
+		{
+			if (! file_exists($path))
+			{
+				//Image is not saved yet
+				$fp = fopen($path, 'x');
+				fwrite($fp, $metadata);
+				fclose($fp);
+			}
+		}
+
 		public function get_images()
 		{
-			//Get already saved images from disk
-			$images = glob($this->image_path . "/*.jpg");
+			$metadata = array();
+			$count = 20;
+			$offset = 0;
 
-			return $images;
+			//How many images should be delivered?
+			if (isset($_GET['count']) && is_numeric($_GET['count']) && $_GET['count'] > 0 && $_GET['count'] < 51)
+				$count = $_GET['count'];
+
+			//From which file do we want to start to count?
+			if (isset($_GET['offset']) && is_numeric($_GET['offset']) && $_GET['offset'] > 0)
+				$offset = $_GET['offset'];
+
+			//Get already saved images from disk
+			$metadata_files = glob($this->image_path . "/*.txt");
+			$metadata_files = array_slice($metadata_files, $offset, $count);
+
+			foreach ($metadata_files as $metadata_file) {
+				$id = substr(basename($metadata_file), 0, strlen(basename($metadata_file)) - 4);
+
+				$fp = fopen($metadata_file, "r");
+				$content = fread($fp, filesize($metadata_file));
+				fclose($fp);
+
+				$content = explode("###", $content);
+				$current_data['created_time'] = $content[0];
+				$current_data['caption'] = $content[1];
+
+				$image_path = $this->image_path . $id . '.jpg';
+				$thumbnail_path = $this->image_path . $id . '-thumb.jpg';
+
+				if (file_exists($image_path) && file_exists($thumbnail_path))
+				{
+					$current_data['image_url'] = $this->opus->config->path_to_url($image_path);
+					$current_data['thumbnail_url'] = $this->opus->config->path_to_url($thumbnail_path);
+
+					$metadata[] = $current_data;
+				}			
+			}
+
+			echo '<pre>';
+			print_r($metadata);
+
 		}
 
 	}
