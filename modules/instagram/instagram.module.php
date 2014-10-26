2cd77eb93abf6aa3e07724ae9b7902793224659b
Tony Gustafsson <mail@tonyg.se>
2014-10-26 16:01:33 +0100

    Instagram Module now saves index in ONE files instead of one per image for performance reasons. It shows the number of images downloaded and creates a link for "next download" for simpler index creating.

--- modules/instagram/instagram.module.php
+++ modules/instagram/instagram.module.php
@@ -6,12 +6,97 @@
 		{	
 			$this->opus =& opus::$instance;
 			
+			$this->index_image_delimiter = "\r\n";
+			$this->index_value_delimiter = "###";
+			$this->index_file = $this->opus->config->instagram->image_path . 'index.txt';
+
 			$this->url_accessible = array('save_images', 'get_images');
+		}
+
+		public function get_images($count = FALSE, $offset = FALSE)
+		{
+			$output = array();
+			$count = ($count !== FALSE) ? $count : $this->opus->config->instagram->no_images;
+			$offset = ($offset !== FALSE) ? $offset : 0;
+
+			$index = $this->get_index($count, $offset);
+
+			foreach ($index as $image) {
+				$current_data['created_time'] = $image['date'];
+				$current_data['caption'] = $image['caption'];
+
+				$large_image_path = $this->opus->config->instagram->image_path . $image['id'] . '-large.jpg';
+				$medium_image_path = $this->opus->config->instagram->image_path . $image['id'] . '-medium.jpg';
+				$small_image_path = $this->opus->config->instagram->image_path . $image['id'] . '-small.jpg';
+
+				if ($this->opus->config->instagram->save_small_images)
+					$current_data['small_image_url'] = $this->opus->path_to_url($small_image_path);
+
+				if ($this->opus->config->instagram->save_medium_images)
+					$current_data['medium_image_url'] = $this->opus->path_to_url($medium_image_path);
+
+				if ($this->opus->config->instagram->save_large_images)
+					$current_data['large_image_url'] = $this->opus->path_to_url($large_image_path);
+
+				$output[] = $current_data;
+			}
+
+			return $output;
+		}
+
+		public function get_index($count = FALSE, $offset = FALSE)
+		{
+			//Read the index file and delivers an array
+			if (! file_exists($this->index_file) || filesize($this->index_file) < 1)
+				return array();
+
+			$fp = fopen($this->index_file, "r");
+			$index_blob = fread($fp, filesize($this->index_file));
+			fclose($fp);
+
+			$index = array();
+			$image_items = explode($this->index_image_delimiter, $index_blob);
+
+			if ($count !== FALSE && $offset !== FALSE)
+				$image_items = array_slice($image_items, $offset, $count);
+
+			foreach ($image_items as $image)
+			{
+				if (empty($image))
+					continue;
+
+				$image_items = explode($this->index_value_delimiter, $image);
+				$image_item_id = $image_items[0];
+				$image_item_date = $image_items[1];
+				$image_item_caption = $image_items[2];
+
+				$index[$image_item_id]['id'] = $image_item_id;
+				$index[$image_item_id]['date'] = $image_item_date;
+				$index[$image_item_id]['caption'] = $image_item_caption;
+			}
+
+			krsort($index);
+
+			return $index;
+		}
+
+		public function save_index($index)
+		{
+			foreach ($index as $key => $val)
+				$index[$key] = implode($this->index_value_delimiter, $val);
+
+			$index = implode($this->index_image_delimiter, $index);
+
+			$fp = fopen($this->index_file, 'w');
+			fwrite($fp, $index . $this->index_image_delimiter);
+			fclose($fp);
 		}
 
 		public function save_images()
 		{
 			$media_url = $this->opus->config->instagram->media_url;
+			$existing_index = $this->get_index();
+			$new_index = array();
 
 			if (isset($_GET['min_id']))
 				$media_url .= '&min_id=' . $_GET['min_id'];
@@ -28,10 +113,9 @@
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 			$data = curl_exec($ch);
+			$json = json_decode($data);
 
 			curl_close($ch);
-
-			$json = json_decode($data);
 
 			foreach ($json->data as $image)
 			{
@@ -39,18 +123,24 @@
 				if ($image->type != "image")
 					continue;
 
-				$image_id = $image->id;
+				if (! array_key_exists($image->id, $existing_index))
+				{
+					//This image is not in the existing index!
+					$created_time = $image->created_time;
+					$caption = (isset($image->caption->text)) ? $image->caption->text : "";
+
+					$new_index[$image->id]['id'] = $image->id;
+					$new_index[$image->id]['date'] = $image->created_time;
+					$new_index[$image->id]['caption'] = (isset($image->caption->text)) ? $image->caption->text : "";
+				}
+
 				$image_small_url = $image->images->thumbnail->url;
 				$image_medium_url = $image->images->low_resolution->url;
 				$image_large_url = $image->images->standard_resolution->url;
-				$created_time = $image->created_time;
-				$caption = (isset($image->caption->text)) ? $image->caption->text : "";
-				$metadata = $created_time . '###' . $caption;
 
-				$metadata_path = $this->opus->config->instagram->image_path . $image_id . '.txt';
-				$image_small_path = $this->opus->config->instagram->image_path . $image_id . '-small.jpg';
-				$image_medium_path = $this->opus->config->instagram->image_path . $image_id . '-medium.jpg';
-				$image_large_path = $this->opus->config->instagram->image_path . $image_id . '-large.jpg';
+				$image_small_path = $this->opus->config->instagram->image_path . $image->id . '-small.jpg';
+				$image_medium_path = $this->opus->config->instagram->image_path . $image->id . '-medium.jpg';
+				$image_large_path = $this->opus->config->instagram->image_path . $image->id . '-large.jpg';
 				
 				if ($this->opus->config->instagram->save_small_images)
 						$this->save_image($image_small_url, $image_small_path);
@@ -60,12 +150,18 @@
 				
 				if ($this->opus->config->instagram->save_large_images)
 						$this->save_image($image_large_url, $image_large_path);
+			}
 
-				$this->save_metadata($metadata_path, $metadata);
+			if (count($new_index) > 0)
+			{
+				$existing_index = array_merge($existing_index, $new_index);
+				$this->save_index($existing_index);
 			}
 
             $this->opus->log->write('info', 'Downloading images from instagram with IP: ' . $_SERVER['REMOTE_ADDR']);
-			echo count($json->data) . ' new photos were downloaded.';
+			echo count($new_index) . ' new photos were downloaded.';
+			end($existing_index);
+			echo ' <a href="' . $this->opus->url('instagram/save_images?max_id=' . key($existing_index)) . '">Get more?</a>';
 		}
 
 		public function save_image($url, $path)
@@ -88,112 +184,5 @@
 				fclose($fp);
 			}
 		}
-
-		public function save_metadata($path, $metadata)
-		{
-			if (! file_exists($path))
-			{
-				//Image is not saved yet
-				$fp = fopen($path, 'x');
-				fwrite($fp, $metadata);
-				fclose($fp);
-			}
-		}
-
-		public function new_get_images($count = FALSE, $offset = FALSE)
-		{
-			$metadata = array();
-			$count = ($count !== FALSE) ? $count : $this->opus->config->instagram->no_images;
-			$offset = ($offset !== FALSE) ? $offset : 0;
-
-			//Get already saved images from disk
-			$counter = 0;
-			$metadata_files = array();
-
-			$dir = $this->opus->config->instagram->image_path;
-
-			if (is_dir($dir) && $dh = opendir($dir))
-			{
-				while (($file = readdir($dh)) !== FALSE) {
-					if (substr($file, -4) == '.txt')
-						$metadata_files[] = $dir . $file;
-				}
-			}
-
-			closedir($dh);
-
-			$metadata_files = array_reverse(array_slice($metadata_files, -20));
-
-			foreach ($metadata_files as $metadata_file) {
-				$id = substr(basename($metadata_file), 0, strlen(basename($metadata_file)) - 4);
-
-				$fp = fopen($metadata_file, "r");
-				$content = fread($fp, filesize($metadata_file));
-				fclose($fp);
-
-				$content = explode("###", $content);
-				$current_data['created_time'] = $content[0];
-				$current_data['caption'] = $content[1];
-
-				$large_image_path = $this->opus->config->instagram->image_path . $id . '-large.jpg';
-				$medium_image_path = $this->opus->config->instagram->image_path . $id . '-medium.jpg';
-				$small_image_path = $this->opus->config->instagram->image_path . $id . '-small.jpg';
-
-				if ($this->opus->config->instagram->save_small_images && file_exists($small_image_path))
-					$current_data['small_image_url'] = $this->opus->path_to_url($small_image_path);
-
-				if ($this->opus->config->instagram->save_medium_images && file_exists($medium_image_path))
-					$current_data['medium_image_url'] = $this->opus->path_to_url($medium_image_path);
-
-				if ($this->opus->config->instagram->save_large_images && file_exists($large_image_path))
-					$current_data['large_image_url'] = $this->opus->path_to_url($large_image_path);
-
-				$metadata[] = $current_data;
-			}
-
-			return $metadata;
-		}
-
-		public function get_images($count = FALSE, $offset = FALSE)
-		{
-			$metadata = array();
-			$count = ($count !== FALSE) ? $count : $this->opus->config->instagram->no_images;
-			$offset = ($offset !== FALSE) ? $offset : 0;
-
-			//Get already saved images from disk
-			$metadata_files = glob($this->opus->config->instagram->image_path . "/*.txt", GLOB_NOSORT);
-			krsort($metadata_files);
-			$metadata_files = array_slice($metadata_files, $offset, $count);
-
-			foreach ($metadata_files as $metadata_file) {
-				$id = substr(basename($metadata_file), 0, strlen(basename($metadata_file)) - 4);
-
-				$fp = fopen($metadata_file, "r");
-				$content = fread($fp, filesize($metadata_file));
-				fclose($fp);
-
-				$content = explode("###", $content);
-				$current_data['created_time'] = $content[0];
-				$current_data['caption'] = $content[1];
-
-				$large_image_path = $this->opus->config->instagram->image_path . $id . '-large.jpg';
-				$medium_image_path = $this->opus->config->instagram->image_path . $id . '-medium.jpg';
-				$small_image_path = $this->opus->config->instagram->image_path . $id . '-small.jpg';
-
-				if ($this->opus->config->instagram->save_small_images && file_exists($small_image_path))
-					$current_data['small_image_url'] = $this->opus->path_to_url($small_image_path);
-
-				if ($this->opus->config->instagram->save_medium_images && file_exists($medium_image_path))
-					$current_data['medium_image_url'] = $this->opus->path_to_url($medium_image_path);
-
-				if ($this->opus->config->instagram->save_large_images && file_exists($large_image_path))
-					$current_data['large_image_url'] = $this->opus->path_to_url($large_image_path);
-
-				$metadata[] = $current_data;
-			}
-
-			return $metadata;
-		}
-
 	}
 ?>
